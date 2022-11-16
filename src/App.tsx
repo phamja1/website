bc16b9eaff73f39433893e453648df85a419c91b
Jason Pham <70985186+phamja1@users.noreply.github.com>
2022-11-16 17:14:01 -0500

    Add Floating Counter Card

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import './App.css'
+import CountCard from './Components/CountCard';
+import NavBar from './Components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <CountCard />
     </div>
   );
 }
---
+++ src/Components/CountCard.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import '../styles/CountCard.css';
+
+function CountCard() {
+    const [counter, setCounter] = useState<number>(0);
+
+    const incrementCounter = () => {
+        setCounter(counter + 1);
+    };
+
+    const decrementCounter = () => {
+        setCounter(counter - 1);
+    };
+    return (
+        <div className="App">
+            <div className="floating-card CountCard">
+                <div className="Counter-title">Counter</div>
+                <div style={{fontSize:'30pt'}}>{counter}</div>
+                <div className="buttons">
+                    <button onClick={decrementCounter} className="btn">-</button>
+                    <button onClick={incrementCounter} className="btn">+</button>
+                </div>
+                
+            </div>
+        </div>
+    );
+}
+
+export default CountCard;
