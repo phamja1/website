5c68f6f459453bf419c63555dcc78ba2f514151b
Jason Pham <70985186+phamja1@users.noreply.github.com>
2022-12-26 00:59:02 -0600

    fix formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import React from 'react';
-import './App.css';
-import Routes from './SiteRoutes';
-
+import React from 'react'
+import './App.css'
+import Routes from './SiteRoutes'
 
 function App() {
-    return (
-        <div className="App">
-            <Routes />
-        </div>
-    );
+  return (
+    <div className='App'>
+      <Routes />
+    </div>
+  )
 }
 
-export default App;
+export default App
